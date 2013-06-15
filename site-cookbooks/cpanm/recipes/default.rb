eb309c6028b1dc806f0b575e5cbfda14540c9ffb
u-pyon <yuichi.oikawa@gmail.com>
2013-06-15 01:19:53 +0000

    Add cpanm recipe.

---
+++ site-cookbooks/cpanm/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: cpanm
+# Recipe:: default
+#
+# Copyright 2013, Yuichi Oikawa
+#
+# All rights reserved - Do Not Redistribute
+package 'curl' do
+  action :install
+  notifies :run, 'package[perl-ExtUtils-MakeMaker]', :immediately
+end
+
+package 'perl-ExtUtils-MakeMaker' do
+  action :install
+  notifies :run, 'execute[install-cpanm]', :immediately
+end
+
+execute 'install-cpanm' do
+  command 'curl -L http://cpanmin.us | perl - --sudo App::cpanminus'
+end
